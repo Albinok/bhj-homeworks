c20811022c946b2965e127e107a6ae5ba197554e
Albina <alkisa17@yandex.ru>
2023-03-13 22:46:20 -0400

    work3

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,11 +1,34 @@
-const rotator = Array.from(document.getElementsByClassName('rotator__case'));
 let index = 0;
 let speed = 1000;
 
-let rotatorTimeout = setTimeout(function time() {
-  rotator.forEach(elem => elem.classList.remove('rotator__case_active'));
-  index >= rotator.length - 1 ? index = 0 : index++;
-  rotator[index].classList.add('rotator__case_active');
-  
-  rotatorTimeout = setTimeout(time, speed);
+let rotatorTime = setInterval(function time() {
+  let rotator = document.querySelector('.rotator__case_active');
+  rotator.classList.remove('rotator__case_active');
+
+  if (rotator.nextElementSibling) {
+    let nextRotator = rotator.nextElementSibling;
+    nextRotator.classList.add('rotator__case_active');
+  } else {
+    nextRotator = document.querySelector('.rotator__case');
+    nextRotator.classList.add('rotator__case_active');
+  }
+
 }, speed);
+
+
+
+
+
+
+
+// const rotator = Array.from(document.getElementsByClassName('rotator__case'));
+// let index = 0;
+// let speed = 1000;
+
+// let rotatorTimeout = setTimeout(function time() {
+//   rotator.forEach(elem => elem.classList.remove('rotator__case_active'));
+//   index >= rotator.length - 1 ? index = 0 : index++;
+//   rotator[index].classList.add('rotator__case_active');
+  
+//   rotatorTimeout = setTimeout(time, speed);
+// }, speed);
--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,8 +1,12 @@
 const reveal = Array.from(document.getElementsByClassName('reveal'));
 
-document.addEventListener('scroll', function () {
+window.addEventListener('scroll', function () {
   reveal.forEach(elem => {
     const {top, bottom} = elem.getBoundingClientRect();
-    top > document.innerHeight || bottom < 0 ? elem.classList.remove('reveal_active') : elem.classList.add('reveal_active');
+    if (top > window.innerHeight || bottom < 0 ) {
+      elem.classList.remove('reveal_active');
+    } else { 
+      elem.classList.add('reveal_active');
+    }
   });
-})
+});
